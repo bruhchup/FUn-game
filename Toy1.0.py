3a44c19aca0ba5f059ac3e287d54bd18fbca23a4
bruhchup <115509602+bruhchup@users.noreply.github.com>
2022-10-11 21:40:01 -0400

    Add files via upload

---
+++ Toy1.0.py
@@ -0,0 +1,17 @@
+print ("what do you need?")
+print ("One")
+print ("Two")
+print ("Three")
+
+numer = input()
+
+if numer == "1":
+	print("I love cock")	
+elif numer == "2":
+	print("In my ass")	
+elif numer == "3":	
+	print("Fart fart fart")
+else:
+	print("no no no fuck off")
+
+input()
